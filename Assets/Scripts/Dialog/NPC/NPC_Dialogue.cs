4fb09c721d7a7bf4a048dd15a88f972b65b1ca55
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-18 19:29:40 -0300

    10-commit

--- Assets/Scripts/Dialog/NPC/NPC_Dialogue.cs
+++ Assets/Scripts/Dialog/NPC/NPC_Dialogue.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class NPC_Dialogue : MonoBehaviour
 {
+    public float dialogueRange;
+    public LayerMask playerLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        if (hit != null)
+        {
+            Debug.Log("player perto");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
     }
 }
