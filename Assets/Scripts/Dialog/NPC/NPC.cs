c3fef60de3983169091f447303304a9016e20065
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-20 17:12:43 -0300

    14-commit

--- Assets/Scripts/Dialog/NPC/NPC.cs
+++ Assets/Scripts/Dialog/NPC/NPC.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    public float speed;
+    public int index;
+    public List<Transform> paths = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,30 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, paths[index].position, speed * Time.deltaTime );
+
+        if(Vector2.Distance(transform.position, paths[index].position) < 0.1f)
+        {
+            if (index < paths.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+
+        Vector2 direction = paths[index].position - transform.position;
+
+        if (direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
