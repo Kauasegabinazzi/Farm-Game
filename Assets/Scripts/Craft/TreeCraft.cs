f5dc8a30a172965c74600534130b6a90cadce7a8
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-25 21:17:59 -0300

    18-commit

--- Assets/Scripts/Craft/TreeCraft.cs
+++ Assets/Scripts/Craft/TreeCraft.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tree : MonoBehaviour
@@ -7,24 +8,25 @@ public class Tree : MonoBehaviour
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnHit()
     {
         treeHealth--;
 
+        anim.SetTrigger("IsHit");
+
         if(treeHealth <= 0)
         {
             //cria o toco e instancia os drops
+            anim.SetTrigger("IsCut");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("axe"))
+        {
+            OnHit();
         }
     }
 }
